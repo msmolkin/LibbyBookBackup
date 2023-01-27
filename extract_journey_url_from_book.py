e7cefee334f9f2d5f31a018ec39358308ddf3345
Michael <michael@smolkin.org>
2023-01-27 00:20:44 -0500

    Update profile. Get the names of each book

--- extract_journey_url_from_book.py
+++ extract_journey_url_from_book.py
@@ -5,23 +5,33 @@
 # }
 
 from pprint import pprint
+import re
 from bs4 import BeautifulSoup as bs
 
 with open("webpage.html", "r") as f:
     soup = bs(f, "html.parser")
 
 all_books = soup(class_="title-plank-details")
+books = []
 
-# print(all_books)
+for book_html in all_books:
+    book = {}
 
-for book in all_books:
-    a = book("a")
-    # print(a[0])
-    url = book("a[href]")
+    base = "https://libbyapp.com"
+    book["url"] = base + book_html.find("a", "title-plank-journey")["href"]
 
-    tpa_aria_label = book.find("a", "title-plank-action").attrs["aria-label"]
-    book_type = tpa_aria_label[:tpa_aria_label.index(":")]  # book.ariaLabel.slice(0, book.ariaLabel.indexOf(":"))
+    tpa_aria_label = book_html.find("a", "title-plank-action").attrs["aria-label"]
+    book["type"] = tpa_aria_label[:tpa_aria_label.index(":")]  # book.ariaLabel.slice(0, book.ariaLabel.indexOf(":"))
     
-    book_title = a[0].find("span", class_ = "title-plank-title").text
-    # console.log(book.href, book.ariaLabel.slice(0, book.ariaLabel.indexOf(":")), book.firstChild.innerText)
-    exit()
+    book["title"] = book_html("a")[0].find("span", class_ = "title-plank-title").text
+    book["title"] = re.sub("&nbsp;|\xa0", " ", book["title"])
+    book["title"] = re.sub("'|\"|\p?", "", book["title"])
+
+    # I could pull the ID from div[class="cover-box-clip"], img[data-cover-slug="63301"], but this easier
+    book["id"] = book["url"][book["url"].rindex("/") + 1:]
+
+    books.append(book)
+    # books[book["id"]] = book
+
+pprint(books)
+print(len(books))
--- open_libby_in_headless_chrome.py
+++ open_libby_in_headless_chrome.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.options import Options
 
 # initializing webdriver for Chrome
 chrome_options = Options()
-user_agent, profile_location = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36", "/Users/michael/Library/Application Support/Google/Chrome/"
-chrome_options.arguments.extend([f"user-agent={user_agent}", f"user-data-dir={profile_location}", "--headless"])
+user_agent, profile_location = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36", "/Users/michael/Library/Application Support/Google/Chrome/LibbyProfile"
+chrome_options.arguments.extend([f"user-agent={user_agent}", f"user-data-dir={profile_location}"]) # , "--headless"])
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options=chrome_options)
@@ -22,14 +22,38 @@ def open_all_months():
             driver.get(f"https://libbyapp.com/timeline/activities/all,all,all,{yyyy}-{mm}")
             # and do everything under here instead
 
-def open_book():
+def save_file(file_name: str, html: str):
+    with open(file_name, "a") as f:
+        f.write(html)
+
+def open_timeline():
     driver.get("https://libbyapp.com/timeline/activities/all,all,all,2022-12")
     # driver.get("https://www.yahoo.com/")
-    time.sleep(3)
+    time.sleep(300)
+    filename = "webpage.html"
+    save_file(filename, driver.page_source)
     # driver.get_screenshot_as_png()
     driver.save_screenshot("screenshot.png")
 
-open_book()
+    """ Creates data for book
+    book {str}: URL
+
+    Returns:
+    book_data {dict}: {url, book_type, title}
+    """
+def open_book(book) -> None:
+    print(book.href, book.ariaLabel.slice(0, book.ariaLabel.indexOf(":")), book.firstChild.innerText)
+    return
+
+open_timeline()
+
+# using JS to get the data for every book on the page: URL, is it a book or audiobook, book title
+# for (book of $$("a.title-plank-action.halo")) {
+#     console.log(book.href, book.ariaLabel.slice(0, book.ariaLabel.indexOf(":")), book.firstChild.innerText)
+# }
+
+# Note: if "activities/journey" not in URL, then there was a problem with this book
+# https://libbyapp.com/timeline/activities/journey/82669
 
 # closing browser
 driver.close()
