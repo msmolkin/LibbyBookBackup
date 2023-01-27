7cd4f0324aac6fa77cd39a9d829a80065167d70c
Michael <michael@smolkin.org>
2023-01-27 16:33:56 -0500

    Update book memory

--- headless/extract_journey_url_from_book.py
+++ headless/extract_journey_url_from_book.py
@@ -4,30 +4,45 @@
 import re
 from bs4 import BeautifulSoup as bs
 
-# https://libbyapp.com/timeline/activities/all,all,all
-with open("activities.html", "r") as f:
-    soup = bs(f, "html.parser")
-
-all_books = soup(class_="title-plank-details")
 books = {}  # uses dict to save each book once
 
-for book_html in all_books:
-    book = {}
+def read_activities_file_and_extract_books() -> list:
+    """ Creates list of books that had records in the current month
 
-    base = "https://libbyapp.com"
-    book["url"] = base + book_html.find("a", "title-plank-journey")["href"]
 
-    tpa_aria_label = book_html.find("a", "title-plank-action").attrs["aria-label"]
-    book["type"] = tpa_aria_label[:tpa_aria_label.index(":")]  # book.ariaLabel.slice(0, book.ariaLabel.indexOf(":"))
-    
-    book["title"] = book_html("a")[0].find("span", class_ = "title-plank-title").text
-    book["title"] = re.sub("&nbsp;|\xa0", " ", book["title"])
-    book["title"] = re.sub("'|\"|\?", "", book["title"])
+    Returns:
+        books {list}: the books recorded so far
+    """
+    with open("month_activities.html", "r") as f:
+        soup = bs(f, "html.parser")
 
-    # I could pull the ID from div[class="cover-box-clip"], img[data-cover-slug="63301"], but this easier
-    book["id"] = book["url"][book["url"].rindex("/") + 1:]
+    books_on_this_page = soup(class_="title-plank-details")
 
-    books[book["id"]] = book
+    for book_html in books_on_this_page:
+        book = {}
+
+        base = "https://libbyapp.com"
+        book["url"] = base + book_html.find("a", class_="title-plank-journey")["href"]
+
+        tpa_aria_label = book_html.find("a", class_="title-plank-action").attrs["aria-label"]
+        book["type"] = tpa_aria_label[:tpa_aria_label.index(":")]  # book.ariaLabel.slice(0, book.ariaLabel.indexOf(":"))
+        
+        book["title"] = book_html("a")[0].find("span", class_ = "title-plank-title").text
+        book["title"] = re.sub("&nbsp;|\xa0", " ", book["title"])
+        book["title"] = re.sub("'|\"|\?", "", book["title"])
+
+        book["author"] = book_html.find(class_="title-plank-author").text
+
+        # I could pull the ID a bit cleaner from img[data-cover-slug="{book_id}"], but this is easier to code:
+        book["id"] = book["url"][book["url"].rindex("/") + 1:]
+
+        books[book["id"]] = book
+
+        return books
 
 # pprint(books)
 # print(len(books))
+
+if __name__ == "__main__":
+    # test
+    read_activities_file_and_extract_books()
--- headless/open_libby_in_headless_chrome.py
+++ headless/open_libby_in_headless_chrome.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dateutil import relativedelta
 import time
 
 from selenium import webdriver
@@ -9,32 +11,37 @@ import extract_journey_url_from_book
 chrome_options = Options()
 user_agent, profile_location = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36", "/Users/michael/Library/Application Support/Google/Chrome/LibbyProfile"
 chrome_options.arguments.extend([f"user-agent={user_agent}", f"user-data-dir={profile_location}"]) # , "--headless"])
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 
 timeline_url = "https://libbyapp.com/timeline/activities"
-years = [2023, 2022] # replace with datetime.datetime.year and year-1.
-# maybe even just use last 12 months, if that's how they store it
-months = [i + 1 for i in range(12)]
 
-def open_all_months():
-    for yyyy in years:
-        for mm in months:
-            driver.get(f"https://libbyapp.com/timeline/activities/all,all,all,{yyyy}-{mm}")
-            # and do everything under here instead
+
+def create_all_months() -> list:
+    today = datetime.today()
+    res = []
+    month_count = 0
+    while month_count < 12 + 1:
+        date_month = today - relativedelta.relativedelta(months=month_count)
+        res.append(date_month.strftime("%Y-%m"))
+        month_count += 1
+    return res
 
 def save_file(file_name: str, html: str):
     with open(file_name, "a") as f:
         f.write(html)
 
 def open_timeline():
-    driver.get("https://libbyapp.com/timeline/activities/all,all,all,2022-12")
-    time.sleep(3)
-    filename = "activities.html"
-    save_file(filename, driver.page_source)
+    months = create_all_months()
+    for year_month in months:
+        driver.get(f"https://libbyapp.com/timeline/activities/all,all,all,{year_month}")
+        time.sleep(3)
+        filename = "month_activities.html"
+        save_file(filename, driver.page_source)
 
-    print(extract_journey_url_from_book.books)
+        extract_journey_url_from_book.read_activities_file_and_extract_books()
+        print(year_month, extract_journey_url_from_book.books)
 
     """ Creates data for book
     book {str}: URL
