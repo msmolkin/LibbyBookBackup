b846de56a86f290ad7b3108a4994786ea3c85f8b
Michael <michael@smolkin.org>
2023-01-29 05:11:48 -0500

    Make headless buttons run, but not clickable

--- headless/extract_export_files_from_journey.py
+++ headless/extract_export_files_from_journey.py
@@ -8,20 +8,21 @@ with open("book_journey.html", "r") as f:
 # tpa_aria_label = book_html.find("a", class_="title-plank-action").attrs["aria-label"]
 
 # section with the "Export Your Data" as CSV/HTML/JSON buttons
-section = journey_html.find("div", class_="popover-choices popover-scroller native-scrollable native-scrollable-y popover-content halos-anchor")
+section = journey_html.find("div", class_ = "popover-choices popover-scroller native-scrollable native-scrollable-y popover-content halos-anchor")
 for export_item in ("Export Your Data", "Table", "CSV"):
     # All possible items (but would waste processing power): ("Export Your Data", "Table", "HTML", "Spreadsheet", "CSV", "Data", "JSON"):
     if not export_item in section.text:
         raise RuntimeError("In book \"" + journey_html.title.text + "\": Searching in the wrong place for HTML and CSV and JSON file export buttons.")
 
 
-popover_buttons = journey_html.find_all("button", class_="popover-back-button halo")
-spans = journey_html.find_all("span", class_="popover-back-button halo")
-# <span role="text">Export Your Data</span><span id="shibui-element-0001" data-access="spoken" role="text">Back to previous popover.</span></div></button><a href="#" class="halo" role="button"><span role="text">Table</span><span class="popover-choice-indicator"><span role="text">HTML</span></span></a><a href="#" class="halo" role="button"><span role="text">Spreadsheet</span><span class="popover-choice-indicator"><span role="text">CSV</span></span></a><a href="#" class="halo" role="button"><span role="text">Data</span><span class="popover-choice-indicator"><span role="text">JSON</span></span></a></div></div></div><button class="shibui-shield halo" role="button" type="button" data-halo-class="shield" data-halo-inset="-1" aria-label="Close popover.">
+# now it should be one of these:
+export_spans_html = section.find_all("span", class_ = "popover-choice-indicator") # okay
+export_buttons_html = section.find_all("a", class_ = "halo", attrs="role='button'") # okay
 
-export_links_html = [button for button in popover_buttons if button.attrs["aria-label"] == "Export Your Data"]
-export_links_html = [span for span in popover_buttons if span.attrs["aria-label"] == "Export Your Data"]
+# for span in export_spans_html:
+#     print(span) # span.click()
 
-print(export_links_html[0].text)
+# for button in export_buttons_html:
+#     button.click()
 
 # tpa_aria_label = book_html.find("a", class_="title-plank-action").attrs["aria-label"]
