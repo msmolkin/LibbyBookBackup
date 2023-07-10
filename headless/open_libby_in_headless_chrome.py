f1c83edc5e4a40223dae34c5d9b087836a4d6647
Michael <michael@smolkin.org>
2023-07-10 13:26:48 -0700

    Whatever updates I made on May 17

--- headless/open_libby_in_headless_chrome.py
+++ headless/open_libby_in_headless_chrome.py
@@ -11,7 +11,7 @@ import extract_journey_url_from_book
 chrome_options = Options()
 user_agent, profile_location = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36", "/Users/michael/Library/Application Support/Google/Chrome/LibbyProfile"
 chrome_options.arguments.extend([f"user-agent={user_agent}", f"user-data-dir={profile_location}"]) # , "--headless"])
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 
