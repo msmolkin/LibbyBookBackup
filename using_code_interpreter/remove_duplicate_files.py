6a5ab26fdc124f86ac133bb1898139bb8fef4887
Michael <michael@smolkin.org>
2023-07-27 01:11:53 -0700

    Prettify remove_duplicates.py output

--- using_code_interpreter/remove_duplicate_files.py
+++ using_code_interpreter/remove_duplicate_files.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from datetime import datetime
 
 directory = "books"
+num_files = len([filename for filename in os.listdir(directory) if filename.startswith("Book") and filename.endswith(".json")])
 
 # Store file contents and their respective paths
 file_contents = defaultdict(list)
@@ -11,9 +12,15 @@ file_contents = defaultdict(list)
 for filename in os.listdir(directory):
     if filename.startswith("Book") and filename.endswith(".json"):
         filepath = os.path.join(directory, filename)
+        book_title = " ".join(filepath.split(" ")[3:filepath.split(" ").index("by")])
 
-        with open(filepath, 'r') as file:
-            file_content = json.load(file)
+        print(f"Reading book {book_title} ({len(file_contents) + 1} / {num_files})")
+
+        try:
+            with open(filepath, 'r') as f:
+                file_content = json.load(f)
+        except TimeoutError:
+            print("If OneDrive is not running. Please start it and try again.")
         
         # Exclude the download date from the filename for comparison
         comparable_filename = " ".join(filename.split(" ")[:-4])
@@ -21,13 +28,19 @@ for filename in os.listdir(directory):
         # Add the file content and filepath to the dictionary
         file_contents[(comparable_filename, str(file_content))].append(filepath)
 
+deleted_files_count = 0
 # Check for duplicates and remove the newer files
 for files in file_contents.values():
     if len(files) > 1:
         # Sort the filepaths by the download date and time in descending order
-        files.sort(key=lambda x: datetime.strptime(x.split(" ")[-2], "%Y-%m-%d %H-%M"), reverse=True)
+        files.sort(key=lambda x: datetime.strptime(x.split(" ")[-2] + " " + x.split(" ")[-1].rstrip(").json"), "%Y-%m-%d %H-%M"), reverse=True)
+        
         
         # Remove all but the first (oldest) file
-        for file in files[1:]:
-            os.remove(file)
-            print(f"Deleted file {file}")
+        # keep a count of the number of files deleted. print it each time
+        for f in files[1:]:
+            os.remove(f)
+            deleted_files_count += 1
+            print(f"Deleted file {f} ({deleted_files_count})")
+
+print(f"Deleted {deleted_files_count} files.")
