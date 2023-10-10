ab2746c8679b94a22f95a90ec2da69c6c33c53d7
Michael <michael@smolkin.org>
2023-10-10 13:13:27 -0700

    download_book_json_synchronous.js:Enhance URL handling
    - Add method to build reading_journey_url from book data if URL is absent
    - Introduce check for 'undefined' within journeyUrl, log error if found
    - Improve error logging for better debugging and traceability

--- using_code_interpreter/download_book_json_synchronous.js
+++ using_code_interpreter/download_book_json_synchronous.js
@@ -58,7 +58,7 @@ async function saveJson(data, folder = "books") {
 
     const page = await browser.newPage();
 
-    // Set the request authorization headers, as per your uploaded headers
+    // Set the request authorization headers
     await page.setExtraHTTPHeaders({
         // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJyZWFkaXZlcnNlIiwiaXNzIjoic2VudHJ5IiwiY2hpcCI6eyJpZCI6Ijg2OWU2NmNkLTQ5ZjYtNGM2My05MzM3LTJlMjM3NGU1ZTk2OCIsInByaSI6IjA3MTc4YmU1LTAxNDYtNDgzZC04ZTlmLTk2M2EyMzFiZWJiMCIsImFnIjo5MDk0NTU5LCJhY2NvdW50cyI6W3siYWciOjkwOTQ1NTksImlkIjoiMTkyOTg3NDciLCJ0eXAiOiJsaWJyYXJ5IiwiY2FyZHMiOlt7ImlkIjoiNzE1NzE3MiIsIm5hbWUiOiIyMTkwMTAxNzQyNDU4NSIsImxpYiI6eyJpZCI6IjM3NyIsImtleSI6ImNjYyJ9fV19LHsiYWciOjkwOTQ1NTksImlkIjoiNjQ4OTgwMjIiLCJ0eXAiOiJsaWJyYXJ5IiwiY2FyZHMiOlt7ImlkIjoiNDg4MzQ4NzIiLCJuYW1lIjoiMiAxMTExIDAyMzkzIDY3MDEiLCJsaWIiOnsiaWQiOiI4OSIsImtleSI6ImJyb29rbHluIn19XX0seyJhZyI6OTA5NDU1OSwiaWQiOiI0MjA1NTQ5OCIsInR5cCI6ImxpYnJhcnkiLCJjYXJkcyI6W3siaWQiOiIyNzM3MzM5OSIsIm5hbWUiOiIyMTIyMzAzNDk4MjEwMSIsImxpYiI6eyJpZCI6IjM3MiIsImtleSI6InNmcGwifX1dfSx7ImFnIjo5MDk0NTU5LCJpZCI6IjY4MjU0OTczIiwidHlwIjoibGlicmFyeSIsImNhcmRzIjpbeyJpZCI6IjUyMTYyMTgzIiwibmFtZSI6bnVsbCwibGliIjp7ImlkIjoiMTAwNzY4Iiwia2V5IjoiY2FuZGlkIn19XX0seyJhZyI6OTA5NDU1OSwiaWQiOiI3NDc5Mjc5MSIsInR5cCI6ImxpYnJhcnkiLCJjYXJkcyI6W3siaWQiOiI1ODY0Mjg4NSIsIm5hbWUiOiIyIDMzMzMgMTIyNDggMDQ2OCIsImxpYiI6eyJpZCI6IjM3Iiwia2V5IjoibnlwbCJ9fV19XX19.cbtriZC14BX05r73PX_-Fvl_iSNy_8cl9nixFw2vgKannojHN8l_eon_2rsTY_sqHRwtO9O8enk7WZgPDY53XMSXEj8SnJ3TZhqrfJAYBRJjYT5Maev4aZeClgDoZ7lnTSxiiES-kh01mFsJvZozm7exvO1UpJzDXAgBUSisp6I677n5jF3aa8ZAzX5r5VFpI3zV_s5dMLlPdYlBuvOPZu4O_t6Ai0imia9nGS8k4DV32MeHL56igaOlhH05NL4vIy8JYvFkkc7fjaF4YvFDdXKZDkWTfNEDdLJAoqeg7MBXy6ZAlNgv9WusViROa5NIEeGKhZ-iyQErn38q5HmOJA',
         'Connection': 'keep-alive',
@@ -82,10 +82,17 @@ async function saveJson(data, folder = "books") {
 
     // Visit each book's reading journey URL and export the data
     for (let book of timelineJson.timeline) {
-
+        if (!book.reading_journey_url) {
+            book.reading_journey_url = `${book.library.url}/similar-${book.title.titleId}/page-1/${book.title.titleId}/journey/${book.title.titleId}`;
+        }
         const journeyUrl = book.reading_journey_url;
+        
+        if (journeyUrl.includes('undefined')) {
+            console.error(`In the book ${book.title.text} (${book.title.titleId}), the journeyUrl has undefined in it: ${journeyUrl}`);
+            continue;
+        }
 
-        await page.goto(journeyUrl, { waitUntil: 'networkidle0' });
+        await page.goto(journeyUrl, { waitUntil: 'networkidle0' }); // Crashes if journeyUrl is not defined
 
         // TODO: figure out why this errors out when not doing it manually and slowly
         for (let selector of ['#shelf-actions-pill-0001 > span', 'a:nth-of-type(2) > span', 'a:nth-of-type(3) > span:nth-of-type(1)']) {
@@ -121,11 +128,11 @@ async function saveJson(data, folder = "books") {
         }
 
 
-        // // Add the download URL to the JSON data
-        // // this doesn't work because readingJourneyJsonUrl is not defined when the JSON is opened
+        // Add the download URL to the JSON data
+        // this doesn't work because readingJourneyJsonUrl is not defined when the JSON is opened
         book["download_url"] = readingJourneyJsonUrl;
 
-        // // Save JSON data
+        // Save JSON data
         await saveJson(readingJourneyJsonUrl);
 
     }
