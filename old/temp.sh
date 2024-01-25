c66c52b1eacb9810bb968e9215b9a4db5337ed9a
Michael <michael@smolkin.org>
2024-01-25 13:24:10 -0800

    Delete unnecessary files and folders

--- old/temp.sh
+++
@@ -1,6 +0,0 @@
-git mv 'using_code_interpreter/Export data from reading journey chrome recording.json' old/using_code_interpreter
-git mv 'using_code_interpreter/Export data from reading journey from Chrome recording.js' old/using_code_interpreter
-git mv 'using_code_interpreter/attempt 1 - gpt response.py' old/using_code_interpreter
-git mv 'using_code_interpreter/attempt 1.prompt' old/using_code_interpreter
-git mv 'using_code_interpreter/attempt 2 how it should have been sent.md' old/using_code_interpreter
-git mv 'using_code_interpreter/download_book_json_synchronous_unknown_what_this_file_is.js' old/using_code_interpreter
