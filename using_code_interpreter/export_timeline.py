25c855c7d4745a22ba3a47fd2e7ca8a9b0704c9f
Michael <michael@smolkin.org>
2024-09-02 10:01:51 -0700

    fix: Update libby timeline export to match their new export procedures
    feat: Add export log functionality to export_timeline.py
    
    This commit adds the ability to log the URL that was attempted to be exported to a file. The log file path is hardcoded for now, but it can be updated later. This feature will help track the URLs that were exported and can be used for further analysis.

--- using_code_interpreter/export_timeline.py
+++ using_code_interpreter/export_timeline.py
@@ -5,7 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 # import time
+
+def log_to_file(url):
+    log_file_path = "/Users/michael/Library/CloudStorage/OneDrive-Personal/Documents/2023/projects/forfun/LibbyBookBackup/export_log.txt"
+    with open(log_file_path, "a") as log_file:
+        log_file.write(f"Exported URL: {url}\n")
 
 def export_timeline():
     # Initialize WebDriver
@@ -24,9 +30,32 @@ def export_timeline():
     wait = WebDriverWait(driver, 5)  # 5 seconds timeout
     actions_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#shelf-actions-pill-0001 > span')))
     actions_button.click()
+    
+    # Sync Timeline
+    sync_timeline_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.arena-overlay a:nth-of-type(1)')))
+    sync_timeline_button.click()
 
+    driver.get("https://libbyapp.com/shelf/timeline/all,loans,all")
+
+    # Wait for the Actions button
+    wait = WebDriverWait(driver, 5)  # 5 seconds timeout
+    actions_button_selectors = [
+        '#shelf-actions-pill-0001 > span',
+        '#shelf-actions-pill-0002 > span',
+        '#shelf-actions-pill-0003 > span'
+    ]
+
+    for selector in actions_button_selectors:
+        try:
+            actions_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
+            break
+        except TimeoutException:
+            continue
+    else:
+        raise TimeoutException("Actions button not found.")
+    actions_button.click()
     # Click Export Timeline
-    export_timeline_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.arena-overlay a:nth-of-type(2)')))
+    export_timeline_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Export Timeline']")))
     export_timeline_button.click()
 
     # Click JSON Data export button
@@ -35,6 +64,7 @@ def export_timeline():
 
     # Save the webpage as a JSON file
     wait.until(lambda d: d.current_url.find("data") == 27)
+    log_to_file(driver.current_url)  # Log the URL I attempted to export to a file. Eventually I'll want to check the file to see if it's always the same one for any given user.
     if driver.current_url.find("data") == 27:
         current_url = driver.current_url
     else:
